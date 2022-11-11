82859d70ce77e5d5db2391cba406e082e4665fb8
jananeesaranraj <jananee.saran@gmail.com>
2022-11-11 12:10:59 -0500

    updated the changes

---
+++ EmployeeSQL/Table_schema.sql
@@ -0,0 +1,52 @@
+DROP TABLE departments;
+-- Creating table departments
+CREATE TABLE departments(
+         dept_no varchar PRIMARY KEY,
+         dept_name varchar);
+select * from departments;		 
+-- Creating table employees
+CREATE TABLE employees(
+        emp_no int NOT NULL,
+        emp_title varchar,
+        birth_date date NOT NULL,
+        first_name varchar,
+        last_name varchar,
+        sex varchar,
+        hire_date date,
+        PRIMARY KEY (emp_no));
+		
+----view the table
+SELECT * FROM  employees;
+
+
+DROP TABLE dep_emp;		
+-- Creating table department employee
+CREATE TABLE dep_emp(
+       emp_no int NOT NULL, 
+       FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+       depT_no varchar NOT NULL,
+	   FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	  );
+	  
+DROP TABLE dep_manager;
+-- Creating table department manager
+CREATE TABLE dep_manager(
+       dept_no varchar NOT NULL,
+       FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+       emp_no int NOT NULL,
+       FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+       );
+	   
+-- Creating table titles
+CREATE TABLE titles(
+	title_id varchar NOT NULL,
+    title Varchar NOT NULL
+);
+
+DROP TABLE salaries;
+-- Creating table salaries
+CREATE TABLE salaries(
+	emp_no Integer NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    salary Varchar NOT NULL
+);
